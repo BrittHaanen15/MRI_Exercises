78b04e055c03eb66a87982a99b43e13ae92abac7
BrittHaanen15 <bshaanen@uio.no>
2025-03-13 19:28:00 +0100

    Finished until 3, started 4

--- Colloquium assignment/Code.m
+++ Colloquium assignment/Code.m
@@ -4,16 +4,16 @@
 I = ones(64);
 [x,y] = meshgrid(1:64);
 
-%circle
-I((x - 32).^2 + (y - 32).^2 < 100) = 0;
+% %circle
+% I((x - 32).^2 + (y - 32).^2 < 100) = 0;
 
 % %point source
 % I(32,32) = 0;
 
-% %diagonal line
-% for i = 1:size(I,1)
-%     I(i,i) = 0;
-% end
+%diagonal line
+for i = 1:size(I,1)
+    I(i,i) = 0;
+end
 
 %Plot image from top
 figure
@@ -51,3 +51,51 @@ xlabel('k_x')
 ylabel('k_y')
 zlabel('A')
 title('k-space, 3D view')
+
+
+%%%%%%%%%%%%%%%% PART 3 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Generate kx, ky, M
+[kx, ky] = meshgrid(-31:32);
+M = zeros(64);
+
+%Calculate k space
+for i = 1:numel(kx)
+    for j = 1:numel(I)
+        M(i) = M(i) + I(j)*exp((-2*pi*1i*(x(j)*kx(i) + y(j)*ky(i)))/64);
+    end
+end
+
+
+%Display result in 2D
+figure
+imagesc(abs(M))
+xlabel('k_x')
+ylabel('k_y')
+title('Manual k-space, 2D view')
+
+%Display result in 3D
+figure
+mesh(abs(M))
+xlabel('k_x')
+ylabel('k_y')
+zlabel('A')
+title('Manual k-space, 3D view')
+
+%Inverse FT to revert to image
+M_inv = ifft2(M);
+
+%Plot image from top
+figure
+surf(abs(M_inv))
+xlabel('x')
+ylabel('y')
+title('Image from reverse FT, 2D view')
+view(0,90)
+
+%Plot image in 3D
+figure
+surf(abs(M_inv))
+xlabel('x')
+ylabel('y')
+zlabel('z')
+title('Image from reverse FT, 3D view')
---
+++ Colloquium assignment/Part4.m
@@ -0,0 +1,106 @@
+%Generate image space with circle
+R = 100;
+I = ones(64);
+[x,y] = meshgrid(1:64);
+I((x - 32).^2 + (y - 32).^2 < R) = 0;
+
+%Generate magnetic field
+B = ones(64);
+D = 0.00001;
+
+for i = 1:numel(B)
+    Z = ((2*(x(i)-1))/64) - 1;
+    X = ((2*(y(i)-1))/64) - 1;
+    delta_B = D*(R^2)*((Z^2 - X^2)/((Z^2 + X^2)^2));
+    B(i) = B(i) + delta_B;
+end
+B((x - 32).^2 + (y - 32).^2 < R) = 0;
+
+%Compute K space
+%Generate kx, ky, M
+[kx, ky] = meshgrid(-31:32);
+M_xshift = zeros(64);
+
+%Calculate k space
+for i = 1:numel(kx)
+    for j = 1:numel(I)
+        M_xshift(i) = M_xshift(i) + I(j)*(exp((-2*pi*1i*(x(j)*kx(i) + y(j)*ky(i)))/64))*(exp((-2*pi*1i*B(j)*kx(i))/64));
+    end
+end
+
+%Display result in 2D
+figure
+imagesc(abs(M_xshift))
+xlabel('k_x')
+ylabel('k_y')
+title('Manual k-space with x shift, 2D view')
+
+%Display result in 3D
+figure
+mesh(abs(M_xshift))
+xlabel('k_x')
+ylabel('k_y')
+zlabel('A')
+title('Manual k-space with x shift, 3D view')
+
+%Inverse FT to revert to image
+M_inv = ifft2(M_xshift);
+
+%Plot image from top
+figure
+surf(abs(M_inv))
+xlabel('x')
+ylabel('y')
+title('Image from reverse FT with x shift, 2D view')
+view(0,90)
+
+%Plot image in 3D
+figure
+surf(abs(M_inv))
+xlabel('x')
+ylabel('y')
+zlabel('z')
+title('Image from reverse FT with x shift, 3D view')
+
+%Do the same for y shift
+M_yshift = zeros(64);
+%Calculate k space
+for i = 1:numel(kx)
+    for j = 1:numel(I)
+        M_yshift(i) = M_yshift(i) + I(j)*(exp((-2*pi*1i*(x(j)*kx(i) + y(j)*ky(i)))/64))*(exp((-2*pi*1i*B(j)*ky(i))/64));
+    end
+end
+
+%Display result in 2D
+figure
+imagesc(abs(M_yshift))
+xlabel('k_x')
+ylabel('k_y')
+title('Manual k-space with y shift, 2D view')
+
+%Display result in 3D
+figure
+mesh(abs(M_yshift))
+xlabel('k_x')
+ylabel('k_y')
+zlabel('A')
+title('Manual k-space with y shift, 3D view')
+
+%Inverse FT to revert to image
+M_inv = ifft2(M_yshift);
+
+%Plot image from top
+figure
+surf(abs(M_inv))
+xlabel('x')
+ylabel('y')
+title('Image from reverse FT with y shift, 2D view')
+view(0,90)
+
+%Plot image in 3D
+figure
+surf(abs(M_inv))
+xlabel('x')
+ylabel('y')
+zlabel('z')
+title('Image from reverse FT with y shift, 3D view')
